c502050afbd5f96f9e48ade4380875af121149b6
Jonathan Farr <jfarr@Jonathans-MacBook-Pro-2.local>
2016-04-05 00:12:46 -0400

    Add file for kmeans code, with function declarations

---
+++ code/kmeans.py
@@ -0,0 +1,15 @@
+#returns the distance between two data points
+def distance(x, y):
+  pass
+
+#partitions the data into the sets closest to each centroid
+def fit(data, centroids):
+  pass
+
+#returns k centroids which partition the data optimally into k clusters
+def cluster(data, k):
+  pass
+
+#allows the user to assign character names to each centroid given
+def label(centroids):
+  pass
