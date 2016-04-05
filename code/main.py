71a4cc938b1833744a034dc080a2135201f70b33
Max Kaplan <mfkaplan@andrew.cmu.edu>
2016-04-04 21:09:11 -0400

    main added

---
+++ code/main.py
@@ -0,0 +1,17 @@
+# LOL
+# TERM PROJECT 701
+# mfkaplan and jfarrus
+
+######################
+# FILE AND HUMAN IO
+######################
+def __main___():
+  return 0
+
+
+#######################
+# PUT KMEANS BELOW HERE
+#######################
+
+
+# poop
