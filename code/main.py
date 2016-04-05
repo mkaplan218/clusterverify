a1bab58c27ae71a3e8094215718adf2e45d9e304
Max Kaplan <mfkaplan@andrew.cmu.edu>
2016-04-05 00:55:44 -0400

    fucking around with numpy

--- code/main.py
+++ code/main.py
@@ -4,22 +4,28 @@
 import argparse
 import numpy as np
 
-# runs main
-if __name__ == '__main__':
-  parser = init_parser()
-  args = parser.parse_args()
-  main(args)
-
 ######################
 # FILE AND HUMAN IO
 ######################
 def main(args):
-  return 0
+  # TODO: figure out how to take in
+  raw_data = np.loadtxt(args.file, dtype = np.str)
+  data = raw_data[ : , 6: ].astype(np.float)
+  labels = raw_data[ : , 1]
+  print(labels.shape)
+  print(data.dtype)
+  counts = {}
+  for elem in np.unique(labels):
+    counts[elem] = 0
+  for elem in labels:
+    counts[elem] += 1
+  print(counts)
+  return 'Exiting nicely'
 
 def init_parser():
   parser = argparse.ArgumentParser(description = 'Some Clustering stuff')
-  parser.add_argument('filepath', required = True, help = 'Data file for clustering')
-  parser.add_argument('-m', required = False, default = 'kmeans',
+  parser.add_argument('file', help = 'Data file path for clustering')
+  parser.add_argument('-m', default = 'kmeans',
       help = 'Method of clustering (default is KMeans)')
   return parser
 
@@ -29,3 +35,11 @@ def init_parser():
 
 
 # poop
+
+
+# runs main
+if __name__ == '__main__':
+  parser = init_parser()
+  args = parser.parse_args()
+  main(args)
+
