11f20888f8aa8fe0973058bd2907dd691a4b564e
Jonathan Farr <jfarr@Jonathans-MacBook-Pro-2.local>
2016-04-05 20:36:12 -0400

    Add test_all method to easily add to suite of tests

--- code/test.py
+++ code/test.py
@@ -1,3 +1,6 @@
+from display import display_matrix
+from parse import parse
+
 def test_display_matrix():
   z_test = [[1, 1, 1], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
   display_matrix(z_test, title = "Test - Z")
@@ -6,3 +9,7 @@ def test_display_data():
   real_data_test = parse("1 o 2 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 1 1 1 1 1 0 0 0 1 0 0 0 1 1 0 1 1 0 0 0 0 1 1 1 0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 1 0 0 0 1 1 1 0 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0", 16, 8)[0]
 
   display_matrix(real_data_test, title = "First real character")
+
+def test_all():
+  test_display_matrix()
+  test_display_data()
